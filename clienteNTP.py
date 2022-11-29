5404fb8b08781b9b36d06e1c881307e7a365779e
Nicol-Huaiquil <72150599+Nicol-Huaiquil@users.noreply.github.com>
2022-11-28 22:10:45 -0300

    Limpiando código parte 1

--- clienteNTP.py
+++ clienteNTP.py
@@ -8,7 +8,9 @@ NTP_PACKET_FORMAT = "!12I"
 NTP_DELTA = 2208988800  # 1970-01-01 00:00:00 hora de referencia
 NTP_QUERY = b'\x1b' + 47 * b'\0'  #Tipo NTP
 
-def ntp_time(host="192.168.1.108", port=123):
+#host="127.0.0.1" Local 
+#host="192.168.1.108"
+def ntp_time(host="127.0.0.1", port=123): 
     with closing(socket( AF_INET, SOCK_DGRAM)) as cliente:
         cliente.sendto(NTP_QUERY, (host, port))
         msg, address = cliente.recvfrom(1024)
--- clienteSNTP.py
+++ clienteSNTP.py
@@ -1,13 +1,15 @@
 import socket, struct, sys, time
 
-NTP_SERVER = '192.168.1.108' #Host_Local="127.0.0.1"
-Puerto = 9001
+SNTP_SERVER="127.0.0.1" #Local
+#SNTP_SERVER = '192.168.1.108' 
+Puerto = 123
+
 TIME1970 = 2208988800
 
 def sntp_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = b'\x1b' + 47 * b'\0'
-    client.sendto(data, (NTP_SERVER, Puerto))
+    client.sendto(data, (SNTP_SERVER, Puerto))
     data, address = client.recvfrom(1024)
     if data:
         print('Response received from:', address)
